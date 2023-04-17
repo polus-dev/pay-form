bd200c7c505aaae11c4b1f5e222526442cf914b2
Melonges <mellonges@icloud.com>
2023-04-17 09:02:25 +0000

    refactor: customEncode stage 1

--- src/utils/customEncode.ts
+++ src/utils/customEncode.ts
@@ -10,6 +10,16 @@ const COMMISION = 0.5;
 const TRANSFER_OPCODE = '05';
 const FAKE_OPCODE = '90';
 
+
+
+interface IEncodeTransfer {
+  txData: string,
+  tokenAddress: string,
+  recipient: string,
+  amount: string
+  uiid: string,
+}
+
 function calculateCommision(amount: number) {
   const commision = (amount * COMMISION) / 100;
   return Math.round(commision);
@@ -18,34 +28,42 @@ function calculateCommision(amount: number) {
 function encodeTransfer(
   token: string,
   recipient: string,
-  amount: string
+  amount: string | number
 ): string {
   const types = ['address', 'address', 'uint256'];
   const encoded = coder.encode(types, [token, recipient, amount]);
   return encoded;
 }
 
-export function encodePay(d: string, amount: number, decumals: number, uiid: string): string {
-  const data = d.slice(10);
+function assertAmount(amount: any): asserts amount is number {
+  if (isNaN(+amount) || +amount <= 0) {
+    throw new Error('Invalid amount');
+  }
+}
+
+export function encodePay({ uiid, amount, txData, recipient, tokenAddress }: IEncodeTransfer): string {
+  assertAmount(amount);
+  const data = txData.slice(10);
   const types = ['bytes', 'bytes[]', 'uint256'];
   const decoded = coder.decode(types, Buffer.from(data, 'hex'));
 
   const commands: string = decoded[0] + TRANSFER_OPCODE + TRANSFER_OPCODE + FAKE_OPCODE;
   const inputs = structuredClone<string[]>(decoded[1]);
   const deadline = decoded[2] as number;
+  const commision = calculateCommision(amount);
 
 
   const transfer = encodeTransfer(
-    USDC_ADDRESS,
-    RECIPIENT,
-    calculateCommision(amount).toString()
+    tokenAddress,
+    recipient,
+    amount - commision
   );
 
 
   const transfer2 = encodeTransfer(
-    USDC_ADDRESS,
-    RECIPIENT2,
-    (amount - calculateCommision(amount)).toString()
+    tokenAddress,
+    recipient, // TODO: change to commision address
+    commision
   );
 
   const uiid_encoded = coder.encode(['uint256', 'bytes'], ['0x' + uiid, '0x00']);
