7c44b6f15ac0cbcddf09b28e09a9013f1cd2775c
melonges <mellonges@icloud.com>
2023-06-19 15:45:26 +0300

    feat(sentry): init

--- src/index.tsx
+++ src/index.tsx
@@ -46,16 +46,14 @@ if (import.meta.env.PROD) {
   Sentry.init({
     dsn: import.meta.env.VITE_REACT_APP_SENTRY_DSN,
     integrations: [
-      new Sentry.BrowserTracing(),
-      new Sentry.Replay({ maskAllText: true, blockAllMedia: false }),
+      new Sentry.BrowserTracing({
+        tracePropagationTargets: [import.meta.env.VITE_REACT_API_URL],
+      }),
+      new Sentry.Replay({ maskAllText: false, blockAllMedia: false }),
     ],
-
-    // Performance Monitoring
-    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
-
-    // Session Replay
-    replaysSessionSampleRate: 1.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    tracesSampleRate: parseFloat(import.meta.env.VITE_REACT_SENTRY_TRACES_SAMPLE_RATE) || 0.1,
+    replaysSessionSampleRate: parseFloat(import.meta.env.VITE_REACT_SENTRY_REPLAYS_SESSION_SAMPLE_RATE) || 0.1,
+    replaysOnErrorSampleRate: parseFloat(import.meta.env.VITE_REACT_SENTRY_REPLAYS_ON_ERROR_SAMPLE_RATE) || 1.0,
   });
 }
 
