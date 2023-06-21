d780b90a5065540a6d2ed0a64698f3cee17be9da
melonges <mellonges@icloud.com>
2023-06-21 19:33:39 +0300

    fix(utils): `roundCryptoAmount` limitation length

--- src/logic/utils.ts
+++ src/logic/utils.ts
@@ -149,21 +149,7 @@ export const getPathFromCallData = (calldata: string) => {
 };
 
 export const roundCryptoAmount = (amount: string) => {
-  let z = "0",
-    p = ".",
-    isStartWithZero = amount.startsWith(z),
-    floatPointIndex = amount.indexOf(p);
-  while (
-    isStartWithZero
-      ? amount[++floatPointIndex] === z
-      : amount[++floatPointIndex] !== z && floatPointIndex < amount.length
-  );
-  return amount.slice(
-    0,
-    amount[floatPointIndex - 1] === p
-      ? floatPointIndex - 1
-      : isStartWithZero
-      ? floatPointIndex + 1
-      : floatPointIndex
-  );
-};
+   let z = "0", o = 4, v = ".", isStartWithZero = amount.startsWith(z), floatPointIndex = amount.indexOf(v);
+   while (isStartWithZero ? amount[++floatPointIndex] === z : amount[++floatPointIndex] !== z && floatPointIndex < amount.length && floatPointIndex <= o);
+   return amount.slice(0, amount[floatPointIndex - 1] === v && !isStartWithZero ? floatPointIndex - 1 : isStartWithZero ? floatPointIndex + 1 : floatPointIndex);
+ };
